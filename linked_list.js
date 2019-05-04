66a0a7a0ae52c40bd9544341e858be059debc994
Linda Mckeith Lam <lam.lindamckeith@gmail.com>
2019-05-04 16:03:14 -0400

    first commit

---
+++ linked_list.js
@@ -0,0 +1,60 @@
+// singly linked list
+// has a head and optional tail
+// inserting in front and tail takes constant time bc you are reassigning pointers
+
+// array
+// inserting and deleting in head takes more time
+// because you have to shift the indices
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insert(value) {
+    if (this.head === null) {
+      this.head = new Node(value);
+      return;
+    }
+    let curr = this.head;
+    while (curr.next) {
+      curr = curr.next;
+    }
+    curr.next = new Node(value);
+  }
+
+  print(curr = this.head) {
+    if (curr === null) {
+      return;
+    } else {
+      console.log(curr.value);
+      this.print(curr.next);
+    }
+  }
+
+  insertAtPos(val, idx, currPos = 0, currNode = this.head) {
+    if (idx === currPos + 1) {
+      let temp = currNode.next;
+      currNode.next = new Node(val);
+      currNode.next.next = temp;
+    } else {
+      this.insertAtPos(val, idx, currPos + 1, currNode.next);
+    }
+  }
+}
+
+const list = new LinkedList();
+list.insert("a");
+list.insert("b");
+list.insert("c");
+list.insert("d");
+console.log(list.print());
+list.insertAtPos("x", 3);
+console.log(list.print());
---
+++ uniq_array.js
@@ -0,0 +1,26 @@
+// write a function that takes in an array and returns another array
+// with only unique elements
+
+const arr2 = ["a", "b", "c", "a"];
+
+function uniq(arr) {
+  let uniq = [];
+  let obj = {};
+
+  arr.forEach(el => {
+    if (el in obj) {
+      obj[el] += 1;
+    } else {
+      obj[el] = 1;
+    }
+  });
+
+  arr.forEach(el => {
+    if (obj[el] === 1) {
+      uniq.push(el);
+    }
+  });
+  return uniq;
+}
+
+console.log(uniq(arr2));
